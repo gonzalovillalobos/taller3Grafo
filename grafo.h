17b5ce6e009904719c268907a726e9845793aa60
gonzalovillalobos <chalopapayero@outlook.com>
2015-09-08 09:23:01 -0300

    creacion de archivos

--- grafo.h
+++ grafo.h
@@ -1,1 +1,13 @@
+#ifndef STDLIB_H
+#include <stdlib.h>
+#define STDLIB_H
+#endif
 
+#ifndef STDIO_H
+#include <stdio.h>
+#define STDIO_H
+#endif
+
+typedef struct t_nodo{
+	
+}
--- main.c
+++ main.c
@@ -1,1 +1,14 @@
+#ifndef STDLIB_H
+#include <stdlib.h>
+#define STDLIB_H
+#endif
 
+#ifndef STDIO_H
+#include <stdio.h>
+#define STDIO_H
+#endif
+
+#ifndef GRAFO_H
+#include "grafo.h"
+#define grafo_H
+#endif
